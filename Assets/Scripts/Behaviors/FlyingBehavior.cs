307e5e41da93f07007fcaab29d5c5fc7bb5c6f9a
Andrey Lurye <ALurye@wsits.com>
2023-07-14 21:39:50 -0400

    Added bump in collision for enemies

--- Assets/Scripts/Behaviors/FlyingBehavior.cs
+++ Assets/Scripts/Behaviors/FlyingBehavior.cs
@@ -9,15 +9,18 @@ public class FlyingBehavior : AthenaMonoBehavior
 
 
     public Vector2 MoveAngle;
-    
-    public bool FacesRight=true;
-    
-    
-    private  Collider _bounds;
-    
+
+    public bool FacesRight = true;
+
+
+    private Collider _bounds;
+
 
     [SerializeField]
     private float speed;
+
+    [SerializeField]
+    private LayerMask bumpsInto;
 
     protected override void Start()
     {
@@ -28,7 +31,7 @@ public class FlyingBehavior : AthenaMonoBehavior
     }
 
     [SerializeField]
-    private float OFFSET_ANGLE= 90;
+    private float OFFSET_ANGLE = 90;
 
     [SerializeField]
     private float SLOWDOWN = 2f;
@@ -37,32 +40,35 @@ public class FlyingBehavior : AthenaMonoBehavior
     {
         MoveAngle = MoveAngle.normalized;
 
-        var deltaSpeed=speed*Time.deltaTime;
+        var deltaSpeed = speed * Time.deltaTime;
 
         var moveDir = new Vector3(MoveAngle.x, 0f, MoveAngle.y);
-        var newPosition=_rigidBody.position+(moveDir * deltaSpeed);
-        if (ColliderUtils.IsPointInsideCollider(_bounds, newPosition))
+        var newPosition = _rigidBody.position + (moveDir * deltaSpeed);
+
+        if (Physics.Raycast(origin: _rigidBody.position, direction: moveDir, out RaycastHit hit, maxDistance: speed * deltaSpeed, layerMask: bumpsInto.value))
         {
-            _rigidBody.position = newPosition;
+            var target = _rigidBody.position - hit.transform.position;
+            var moveAngle = new Vector2(target.x, target.z);
+            moveDir = new Vector3(moveAngle.x, 0f, moveAngle.y);
+            newPosition = _rigidBody.position + (moveDir * deltaSpeed * SLOWDOWN);
         }
-        else
+
+
+        if (!ColliderUtils.IsPointInsideCollider(_bounds, newPosition))
         {
             //moveDir = new Vector3(MoveAngle.x * -1, 0f, 0f);
-            newPosition = _rigidBody.position + (Quaternion.AngleAxis(-OFFSET_ANGLE, Vector3.up) *moveDir * deltaSpeed / SLOWDOWN);
-            if (ColliderUtils.IsPointInsideCollider(_bounds, newPosition))
-            {
-                _rigidBody.position = newPosition;
-            }
-            else
+            newPosition = _rigidBody.position + (Quaternion.AngleAxis(-OFFSET_ANGLE, Vector3.up) * moveDir * deltaSpeed / SLOWDOWN);
+            if (!ColliderUtils.IsPointInsideCollider(_bounds, newPosition))
             {
                 //moveDir = new Vector3(0f * -1, 0f, MoveAngle.y * -1);
                 newPosition = _rigidBody.position + (Quaternion.AngleAxis(OFFSET_ANGLE, Vector3.up) * moveDir * deltaSpeed / SLOWDOWN);
-                if (ColliderUtils.IsPointInsideCollider(_bounds, newPosition))
+                if (!ColliderUtils.IsPointInsideCollider(_bounds, newPosition))
                 {
-                    _rigidBody.position = newPosition;
+                    newPosition = _rigidBody.position;
                 }
             }
         }
+        _rigidBody.position = newPosition;
 
         if (moveDir.x <= 0 == FacesRight)
         {
