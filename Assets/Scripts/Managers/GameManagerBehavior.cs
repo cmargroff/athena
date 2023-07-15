0399fa5512e0e9acda0fdba071f4d700f4e034b0
Andrey Lurye <ALurye@wsits.com>
2023-07-15 19:12:56 -0400

    Added timed event

--- Assets/Scripts/Managers/GameManagerBehavior.cs
+++ Assets/Scripts/Managers/GameManagerBehavior.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManagerBehavior : AthenaMonoBehavior
 {
@@ -10,13 +12,56 @@ public class GameManagerBehavior : AthenaMonoBehavior
     public GameObject Player;
 
 
+    [SerializeField]
+    private Dictionary<Guid,TimedEvent> _timedEvents= new Dictionary<Guid, TimedEvent>();
+
+    private Int64 _frameCount = 0;
+
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
         SafeAssigned(Bounds);
+
+
+        //AddTimedEvent(1f,()=>Debug.Log($"Timed event {Time.timeSinceLevelLoad}"));
     }
 
     // Update is called once per frame
-    
+
+    public TimedEvent AddTimedEvent(float seconds, Action action)
+    { 
+        var te= new TimedEvent() { 
+            Id=Guid.NewGuid(),
+            Action= action,
+            Frames=(int)Math.Floor(seconds/Time.fixedDeltaTime)
+        };
+        _timedEvents.Add(te.Id, te);
+        return te;
+    }
+    public void RemoveTimedEvent(TimedEvent te)
+    {
+        _timedEvents.Remove(te.Id);
+    }
+
+
+    protected override void PausibleFixedUpdate()
+    {
+        foreach (var kv in _timedEvents)
+        {
+            if (_frameCount % kv.Value.Frames == 0)
+            {
+                kv.Value.Action();
+            }
+        }
+        _frameCount++;
+    }
+
+    public class TimedEvent
+    {
+        public Guid Id;
+        public int Frames;
+        public Action Action;
+
+    }
 }
