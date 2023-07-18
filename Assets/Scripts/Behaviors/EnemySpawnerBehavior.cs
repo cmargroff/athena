ec7e0b45f7704358848786a1a0fc42d17c4523f6
Andrey Lurye <ALurye@wsits.com>
2023-07-18 15:02:46 -0400

    Added enemy spawner

---
+++ Assets/Scripts/Behaviors/EnemySpawnerBehavior.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerBehavior : AthenaMonoBehavior
+{
+    [SerializeField]
+    private BoxCollider2D _spawnBoundry;
+    [SerializeField]
+    private float _spawnRate;
+    [SerializeField]
+    private GameObject _enemy;
+
+    private GameManagerBehavior.TimedEvent _timedEvent;
+    //private Vector3 _trueBoundingScale=Vector3.one;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        //var obj = _spawnBoundry.transform;
+        //do
+        //{
+        //    _trueBoundingScale = new  Vector3(_trueBoundingScale.x * obj.localScale.x, _trueBoundingScale.y * obj.localScale.y, _trueBoundingScale.z * obj.localScale.z);
+        //    obj = obj.parent;
+        //} while (obj.parent != null);
+
+        SafeAssigned(_spawnBoundry);
+
+        SetSpawnRate(_spawnRate);
+    }
+
+    private void SetSpawnRate(float rate)
+    {
+        if (_timedEvent == null)
+        {
+            _timedEvent = _gameManager.AddTimedEvent(rate, () =>
+            {
+                var newPosition = GetRandomPointOnBorder(_spawnBoundry);
+                Instantiate(_enemy, newPosition, Quaternion.identity);
+            }, gameObject);
+        } 
+    }
+
+    private Vector2 GetRandomPointOnBorder(BoxCollider2D boxCollider)
+    {
+        // Get the BoxCollider2D's position, size, and rotation
+        Vector2 position = boxCollider.transform.position;
+        Vector2 size = Vector2.Scale(boxCollider.size, boxCollider.transform.localScale);
+        float rotation = boxCollider.transform.eulerAngles.z;
+
+        // Calculate half extents of the rotated BoxCollider2D
+        Vector2 halfExtents = size * 0.5f;
+        float cosRotation = Mathf.Cos(rotation * Mathf.Deg2Rad);
+        float sinRotation = Mathf.Sin(rotation * Mathf.Deg2Rad);
+        float halfWidth = halfExtents.x * Mathf.Abs(cosRotation) + halfExtents.y * Mathf.Abs(sinRotation);
+        float halfHeight = halfExtents.y * Mathf.Abs(cosRotation) + halfExtents.x * Mathf.Abs(sinRotation);
+
+        // Get a random point on the border of the BoxCollider2D
+        Vector2 randomPointLocal = Vector2.zero;
+
+        float side = Random.Range(0, 4); // Randomly select a side of the box
+
+        switch (side)
+        {
+            case 0: // Top side
+                randomPointLocal = new Vector2(Random.Range(-halfWidth, halfWidth), halfHeight);
+                break;
+            case 1: // Bottom side
+                randomPointLocal = new Vector2(Random.Range(-halfWidth, halfWidth), -halfHeight);
+                break;
+            case 2: // Left side
+                randomPointLocal = new Vector2(-halfWidth, Random.Range(-halfHeight, halfHeight));
+                break;
+            case 3: // Right side
+                randomPointLocal = new Vector2(halfWidth, Random.Range(-halfHeight, halfHeight));
+                break;
+        }
+
+        // Rotate the local random point by the BoxCollider2D's rotation
+        Vector2 randomPointRotated = new Vector2(
+            randomPointLocal.x * cosRotation - randomPointLocal.y * sinRotation,
+            randomPointLocal.x * sinRotation + randomPointLocal.y * cosRotation
+        );
+
+        // Transform the rotated point back to world space
+        Vector2 randomPointWorld = position + randomPointRotated;
+
+        return randomPointWorld*.70f;
+    }
+}
+
