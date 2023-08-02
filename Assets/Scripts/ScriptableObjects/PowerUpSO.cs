6bd27019fd194efa6663fb34e049e886770c8a5e
Andrey Lurye <ALurye@wsits.com>
2023-08-02 11:12:32 -0400

    Adding powerup shop

---
+++ Assets/Scripts/ScriptableObjects/PowerUpSO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class PowerUpSO : ScriptableObject
+{
+    [Range(0.9f, 10)]
+    public float Damage = 1f;
+    [Range(0.9f, 10)]
+    public float Knockback = 1f;
+    [Range(0.9f, 10)]
+    public float Speed = 1f;
+    [Range(0.9f, 10)]
+    public float Armor = 1f;
+    //public float AttackSpeed = 1f;
+    //public float AttackDuration = 1f;
+    [Range(0.9f, 10)]
+    public float AttackFrequency = 1f;
+}
+
