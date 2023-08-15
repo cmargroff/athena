f3e8ddc1022e1e06be4afc972cc27c3e3a8ff455
Chris Margroff <sora3087@gmail.com>
2023-08-15 14:44:04 -0400

    refactor pickup animation to use IDeath

--- Assets/Scripts/Behaviors/PickupAnimationBehavior.cs
+++ Assets/Scripts/Behaviors/PickupAnimationBehavior.cs
@@ -1,45 +1,55 @@
-using DG.Tweening;
 using UnityEngine;
 using UnityEditor;
 [RequireComponent(typeof(BaseStateMachineBehavior))]
 [RequireComponent(typeof(PickupBehavior))]
-public  class PickupAnimationBehavior: AthenaMonoBehavior
+public class PickupAnimationBehavior : AthenaMonoBehavior, IDeath
 {
-    public float TravelSpeed = 1f;
-    private GameObject _player;
-    private Vector3 _angleAway;
-    private bool _running = false;
-    private float _travelDuration = 0f;
-    private Vector3 _direction;
-    public void Run()
+  public float TravelSpeed = 1f;
+  private GameObject _player;
+  private Vector3 _angleAway;
+  private bool _running = false;
+  private float _travelDuration = 0f;
+  private Vector3 _direction;
+  public override void OnActive()
+  {
+    base.OnActive();
+    Run();
+  }
+  public void Run()
+  {
+    if (_running) return;
+    _player = _gameManager.Player;
+    _angleAway = (transform.position - _player.transform.position).normalized;
+    _travelDuration = 0f;
+    _running = true;
+  }
+  protected override void PlausibleUpdate()
+  {
+    if (_running)
     {
-        if(_running) return;
-        _player = _gameManager.Player;
-        _angleAway = (transform.position - _player.transform.position).normalized;
-        _travelDuration = 0f;
-        _running = true;
-    }
-    protected override void PlausibleFixedUpdate(){
-        if(_running){
-            var toPlayer = _player.transform.position - transform.position;
-            
-            // if the distance is less than 0.1f, then we're close enough to the player to be picked up
-            if(toPlayer.magnitude < 0.5f){
-                _gameManager.CollectPickup(GetComponent<PickupBehavior>());
-                _running = false;
-                return;
-            }
+      var toPlayer = _player.transform.position - transform.position;
 
-            _direction = Vector3.Lerp(_angleAway, toPlayer, _travelDuration);
-            transform.position += _direction * TravelSpeed * Time.fixedDeltaTime;
+      // if the distance is less than 0.1f, then we're close enough to the player to be picked up
+      if (toPlayer.magnitude < 0.5f)
+      {
+        _gameManager.CollectPickup(GetComponent<PickupBehavior>());
+        _running = false;
+        gameObject.SetActive(false);
+        return;
+      }
 
-            _travelDuration = Mathf.Clamp01(_travelDuration + Time.fixedDeltaTime);
-        }
+      _direction = Vector3.Lerp(_angleAway, toPlayer, _travelDuration);
+      transform.position += _direction * TravelSpeed * Time.deltaTime;
+
+      _travelDuration = Mathf.Clamp01(_travelDuration + Time.deltaTime);
     }
-    private void OnDrawGizmos() {
-        if(_running){
-            Handles.color = Color.cyan;
-            Handles.ArrowHandleCap(0, transform.position, Quaternion.LookRotation(_direction), _direction.magnitude, EventType.Repaint);
-        }    
+  }
+  private void OnDrawGizmos()
+  {
+    if (_running)
+    {
+      Handles.color = Color.cyan;
+      Handles.ArrowHandleCap(0, transform.position, Quaternion.LookRotation(_direction), _direction.magnitude, EventType.Repaint);
     }
+  }
 }
