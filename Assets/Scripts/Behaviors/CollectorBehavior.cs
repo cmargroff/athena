b73c0d9f4fd283fc638a6482f3d88afd9512c08d
Chris Margroff <sora3087@gmail.com>
2023-08-15 13:36:36 -0400

    udpate collector trigger to be on enter

--- Assets/Scripts/Behaviors/CollectorBehavior.cs
+++ Assets/Scripts/Behaviors/CollectorBehavior.cs
@@ -3,15 +3,11 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class CollectorBehavior : AthenaMonoBehavior
 {
-
-    private void OnTriggerStay2D(Collider2D other)
+  private void OnTriggerEnter2D(Collider2D other) {
+    var pickup = other.GetComponent<PickupBehavior>();
+    if (pickup != null)
     {
-        var pickup = other.GetComponent<PickupBehavior>();
-        if (pickup != null)
-        {
-           pickup.Pickup();
-
-
-        }
+      pickup.Pickup();
     }
+  }
 }
