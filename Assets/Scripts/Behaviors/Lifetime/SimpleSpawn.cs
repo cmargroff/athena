cb9b3501f2a753ea701341545a6f26b7b0437e32
Chris Margroff <sora3087@gmail.com>
2023-08-15 14:43:21 -0400

    simple active only spawn

---
+++ Assets/Scripts/Behaviors/Lifetime/SimpleSpawn.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+[RequireComponent(typeof(BaseStateMachineBehavior))]
+internal class SimpleSpawn : AthenaMonoBehavior, ISpawn
+{
+    public override void OnActive()
+    {
+        base.OnActive();
+        _stateMachine.SetState(typeof(IAlive));
+    }
+}
