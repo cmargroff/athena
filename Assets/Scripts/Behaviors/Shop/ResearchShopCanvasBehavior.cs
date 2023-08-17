3c9096f544e220381c7b8f860727b96e8f87a603
Chris Margroff <sora3087@gmail.com>
2023-08-17 17:36:09 -0400

    add research shop behavior

---
+++ Assets/Scripts/Behaviors/Shop/ResearchShopCanvasBehavior.cs
@@ -0,0 +1,43 @@
+
+using UnityEngine;
+public  class ResearchShopCanvasBehavior : ShopCanvasBehavior<PowerUpSO>
+{
+    [SerializeField]
+    private WeaponSO _buildingTurretConfig;
+
+    private ShopBuildingBehavior[] _buildings;
+
+    protected override void Start()
+    {
+        base.Start();
+        _buildings = FindObjectsByType<ShopBuildingBehavior>(FindObjectsSortMode.None);
+        SafeAssigned(_buildingTurretConfig);
+    }
+    protected override string GetTitle()
+    {
+        return "Research Lab";
+    }
+    public override void Buy(PowerUpSO item)
+    {
+        Spend(item);
+
+        foreach (var building in _buildings)
+        {
+            if (building.GetComponent<WeaponBehavior>() == null)
+            {
+                building.gameObject.AddComponent(typeof(BuildingStatAdjust));
+                var weaponBehavior = (BaseWeaponBehavior)building.gameObject.AddComponent(typeof(WeaponBehavior));
+                weaponBehavior.WeaponConfig = _buildingTurretConfig;
+                weaponBehavior.enabled = true;
+            }
+        }
+        _gameManager.BuildingCharacter.Knockback += item.Knockback;
+        _gameManager.BuildingCharacter.Damage += item.Damage;
+
+        if (item.AttackFrequency > 0)
+        {
+            _gameManager.BuildingCharacter.AttackFrequency += item.AttackFrequency;
+            _gameManager.BuildingCharacter.OnStatsChanged?.Invoke();
+        }
+    }
+}
