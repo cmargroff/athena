6737eb819e340dc568841a04c117b1bc5af87e4d
Chris Margroff <sora3087@gmail.com>
2023-08-20 02:19:28 -0400

    add bg and animation to healthbar

--- Assets/Scripts/Behaviors/HUDHealthBehavior.cs
+++ Assets/Scripts/Behaviors/HUDHealthBehavior.cs
@@ -1,18 +1,35 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HUDHealthBehavior : AthenaMonoBehavior
 {
     private Material _mat;
+    private Sequence _seq;
+    private bool _animating;
     private void Start()
     {
-        var img = GetComponentInChildren<Image>();
+        var img = gameObject.FindObjectByName("Fill").GetComponent<Image>();
         img.material = new Material(img.material);
         _mat = img.material;
         _gameManager.PlayerHealthChanged += UpdateHealth;
     }
     private void UpdateHealth(float percent)
     {
-        _mat.SetFloat("_FillAmount", percent);
+        if (_seq != null)
+        {
+            _seq.Kill();
+        }
+        _animating = true;
+        _seq = DOTween.Sequence();
+        _seq.SetUpdate(UpdateType.Manual);
+        _seq.Append(_mat.DOFloat(percent, "_FillAmount", 0.5f));
+    }
+    protected override void PlausibleFixedUpdate()
+    {
+        if (_animating)
+        {
+            _seq.ManualUpdate(Time.fixedDeltaTime, Time.fixedDeltaTime);
+        }
     }
 }
